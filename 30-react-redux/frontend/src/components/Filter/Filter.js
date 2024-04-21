b22354144c5531c6c9de8558c77a6efd3101bc72
Saverzus <saverzus@gmail.com>
2024-04-22 00:29:22 +0300

    send filter redux state on input onChange

--- 30-react-redux/frontend/src/components/Filter/Filter.js
+++ 30-react-redux/frontend/src/components/Filter/Filter.js
@@ -1,9 +1,19 @@
+import { useDispatch } from 'react-redux';
+import { setTitleFilter } from '../../redux/slices/filterSlice'
 import './Filter.css'
 
 const Filter = () => {
+    const dispatch = useDispatch();
+
+    const handleTitleFilterChange = (e) => {
+        dispatch(setTitleFilter(e.target.value))
+    }
+
     return (
         <div className="app-block filter">
-            <h2>Filters</h2>
+            <div className='filter-group'>
+                <input type="text" placeholder='Filter by title...' ohChange={handleTitleFilterChange} />
+            </div>
         </div>
     )
 }
---
+++ 30-react-redux/frontend/src/redux/slices/filterSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    title: '',
+};
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState: initialState,
+    reducers: {
+        setTitleFilter: (state, action) => {
+            state.title = action.payload;
+        },
+    },
+});
+
+
+export const { setTitleFilter } = filterSlice.actions;
+
+export default filterSlice.reducer;
+
